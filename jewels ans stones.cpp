ec47d2e447670bac8ac17cc50de7568fb6bd15b0
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-05-07 16:14:47 +0530

    Create jewels ans stones.cpp

---
+++ jewels ans stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n=jewels.size();
+        int m=stones.size();
+        map<char,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[jewels[i]]=1;
+        }
+        int sum=0;
+        for(int i=0;i<m;i++)
+        {
+            sum+=mp[stones[i]];
+        }
+        return sum;
+    }
+};
