411871e4a6afe744c6e05a2de6b2b9f5d6a4f4c4
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-06-24 22:37:10 +0530

    Create pattern 10.cpp

---
+++ geeks for geeks/pattern/pattern 10.cpp
@@ -0,0 +1,20 @@
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<"*"<<" ";
+                
+            }
+            cout<<endl;
+        }
+         for(int i=n-1;i>0;i--){
+            for(int j=1;j<=i;j++){
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+        // // code here
+    }
+};
