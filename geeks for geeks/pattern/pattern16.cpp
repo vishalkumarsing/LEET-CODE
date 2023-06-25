cf92d44d8318a1d6b9325d4dc1149aa5525c5ffd
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-06-25 22:02:24 +0530

    Create pattern16.cpp

---
+++ geeks for geeks/pattern/pattern16.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            char ch='A'+i;
+            for(int j=0;j<=i;j++){
+                cout<<ch;
+                
+            }
+            cout<<endl;
+        }
+        // code here
+    }
+};
